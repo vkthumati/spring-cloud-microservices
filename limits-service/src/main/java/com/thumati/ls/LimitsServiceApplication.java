92470b13dd9311614831effc9f8ad2fd201ff7b3
vkthumati <info2vasanth@gmail.com>
2018-06-06 17:22:06 -0500

    Added hystrix

--- limits-service/src/main/java/com/thumati/ls/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/thumati/ls/LimitsServiceApplication.java
@@ -2,8 +2,14 @@ package com.thumati.ls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
--- limits-service/src/main/java/com/thumati/ls/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/thumati/ls/controller/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.thumati.ls.config.Configuration;
 import com.thumati.ls.model.LimitConfiguration;
 
@@ -17,5 +18,17 @@ public class LimitsConfigurationController {
 	public LimitConfiguration getLimitsFromConfigurations() {
 		//return new LimitConfiguration(1001,1);
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/limits-fault-tolerance")
+	@HystrixCommand(fallbackMethod="fallbackGetLimitsConfigurations")
+	public LimitConfiguration getLimitsConfigurations() {
+		//return new LimitConfiguration(1001,1);
+		throw new RuntimeException("Not Available");
+		//return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	public LimitConfiguration fallbackGetLimitsConfigurations() {
+		return new LimitConfiguration(9999, 99);
 	}
 }
