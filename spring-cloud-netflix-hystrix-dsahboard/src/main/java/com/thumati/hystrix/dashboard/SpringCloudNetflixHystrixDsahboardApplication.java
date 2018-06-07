de49c609a6a7ecb3dae236bd66d2f066c5cc5a57
vkthumati <info2vasanth@gmail.com>
2018-06-07 12:18:33 -0500

    hystrix dashborad service

---
+++ spring-cloud-netflix-hystrix-dsahboard/src/main/java/com/thumati/hystrix/dashboard/SpringCloudNetflixHystrixDsahboardApplication.java
@@ -0,0 +1,12 @@
+package com.thumati.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudNetflixHystrixDsahboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudNetflixHystrixDsahboardApplication.class, args);
+	}
+}
