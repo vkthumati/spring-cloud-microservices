2ecae4c9fe78e9a64ffe20d2dcec3e8b33abc2a8
vkthumati <info2vasanth@gmail.com>
2018-06-07 18:09:59 -0500

    turbine stream added

--- limits-service/src/main/java/com/thumati/ls/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/thumati/ls/LimitsServiceApplication.java
@@ -3,11 +3,11 @@ package com.thumati.ls;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
--- spring-cloud-netflix-hystrix-dsahboard/src/main/java/com/thumati/hystrix/dashboard/SpringCloudNetflixHystrixDsahboardApplication.java
+++ spring-cloud-netflix-hystrix-dsahboard/src/main/java/com/thumati/hystrix/dashboard/SpringCloudNetflixHystrixDsahboardApplication.java
@@ -2,8 +2,14 @@ package com.thumati.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
 public class SpringCloudNetflixHystrixDsahboardApplication {
 
 	public static void main(String[] args) {
