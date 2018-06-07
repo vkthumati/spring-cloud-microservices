339c6f69b2d5e9f16a3cb1e7b6659c14ed1b2670
vkthumati <info2vasanth@gmail.com>
2018-06-07 09:57:33 -0500

    fixed hystrix dashboard issue

--- limits-service/src/main/java/com/thumati/ls/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/thumati/ls/LimitsServiceApplication.java
@@ -3,11 +3,9 @@ package com.thumati.ls;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 public class LimitsServiceApplication {
